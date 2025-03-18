210eb818b3ac199680bb091fd31e35b109824c4a
Fyodor Cherniy <f.cherniy@easyreport.ai>
2025-03-18 17:16:41 +0400

    replace background in css with background prop

--- src/CustomWebchat/Containers/FullContainer/FullContainer.tsx
+++ src/CustomWebchat/Containers/FullContainer/FullContainer.tsx
@@ -1,14 +1,20 @@
-import { ContainerProps } from '../interface'
-import s from './FullContainer.module.css'
+import { KeycloakContext } from "@/index";
+import { useContext } from "react";
+import { ContainerProps } from "../interface";
+import s from "./FullContainer.module.css";
 
-const FullContainer = ({ header, workArea, input }: ContainerProps) => (
-  <div className={s['wrapper']}>
-    {header}
-    <div className={s['work-area-and-input-wrapper']}>
-      {workArea}
-      <div className={s['input-wrapper']}>{input}</div>
+const FullContainer = ({ header, workArea, input }: ContainerProps) => {
+  const { background } = useContext(KeycloakContext);
+
+  return (
+    <div className={s["wrapper"]}>
+      {header}
+      <div className={s["work-area-and-input-wrapper"]} style={{ background }}>
+        {workArea}
+        <div className={s["input-wrapper"]}>{input}</div>
+      </div>
     </div>
-  </div>
-)
+  );
+};
 
-export default FullContainer
+export default FullContainer;
