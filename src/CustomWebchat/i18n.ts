4fca2dfe8c92650164e105cb1f7ee8194cb7ef89
Fyodor Cherniy <f.cherniy@easyreport.ai>
2025-03-21 12:58:44 +0400

    new i18n, fix missing i18n, remove extraneous .d.ts

--- src/CustomWebchat/i18n.ts
+++ src/CustomWebchat/i18n.ts
@@ -1,28 +1,60 @@
-interface I18N {
-  en: { [key: string]: string };
-  ru: { [key: string]: string };
+import { useContext } from "react";
+import { KeycloakContext } from "..";
+
+type Translations = {
+  [key: string]:
+    | readonly string[]
+    | ((...args: any[]) => readonly string[])
+    | Translations;
+};
+
+const translations = {
+  online: ["Online", "Онлайн"],
+  tryingToReconnect: ["Trying to reconnect...", "Восстановление соединения..."],
+  inputPlaceholder: ["Write a message...", "Напишите сообщение..."],
+  clearMessages: ["Clear messages", "Очистить сообщения"],
+  expandChat: ["Maximize", "На весь экран"],
+  compressChat: ["Minimize", "Уменьшить"],
+  closeChat: ["Close", "Закрыть"],
+} as const satisfies Translations;
+
+const langToIndex = {
+  en: 0,
+  ru: 1,
+} as const;
+
+type ResolvedTranslations<T> = {
+  [K in keyof T]: T[K] extends readonly string[]
+    ? T[K][number]
+    : T[K] extends (...args: infer Args) => readonly string[]
+    ? (...args: Args) => string
+    : ResolvedTranslations<T[K]>;
+};
+
+function resolveTranslations<T extends Translations>(
+  obj: T,
+  lang: keyof typeof langToIndex
+): ResolvedTranslations<T> {
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => {
+      if (Array.isArray(value)) {
+        return [key, value[langToIndex[lang]]];
+      }
+      if (typeof value === "function") {
+        return [
+          key,
+          (...args: any[]) => {
+            const result = value(...args);
+            return result[langToIndex[lang]];
+          },
+        ];
+      }
+      return [key, resolveTranslations(value as Translations, lang)];
+    })
+  ) as ResolvedTranslations<T>;
 }
 
-const I18N_ = {
-  "webChat/title": ["Easy Report Webchat", "Easy Report Веб-чат"],
-  "webChat/online": ["Online", "Онлайн"],
-  "webChat/tryingToReconnect": [
-    "Trying to reconnect...",
-    "Восстановление соединения...",
-  ],
-  "webChat/inputPlaceholder": ["Write a message...", "Напишите сообщение..."],
-  "webChat/dataModel": ["Data model:", "Модель данных:"],
-  clearMessages: ["Clear messages", "Очистить сообщения"],
-};
-
-const WebChatI18N: I18N = {
-  en: {},
-  ru: {},
-};
-
-Object.entries(I18N_).forEach(([key, [en, ru]]) => {
-  WebChatI18N.en[key] = en;
-  WebChatI18N.ru[key] = ru;
-});
-
-export default WebChatI18N;
+export function useI18N() {
+  const { lang } = useContext(KeycloakContext);
+  return resolveTranslations(translations, lang as keyof typeof langToIndex);
+}
