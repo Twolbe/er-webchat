f246b64914f787f0f64fbe8c0c78ae096f9fd113
Fyodor Cherniy <f.cherniy@easyreport.ai>
2025-03-27 18:26:55 +0400

    bump minor semver (considering senderId feature), minor senderId refactor

--- src/index.tsx
+++ src/index.tsx
@@ -53,7 +53,7 @@ export const KeycloakContext = createContext<{
  * @param {*} [title='Easy Report Веб-чат'] - webchat title. Default 'Easy Report Веб-чат'
  * @param {*} extraAction - extra action for header
  * @param {*} [background='#d9d9d9'] - background CSS property. Default '#d9d9d9'
- * @param {*} [senderId=undefined] - external value for senderId
+ * @param {*} senderId - external value for senderId
  */
 const ERWebChat = ({
   getTokens,
